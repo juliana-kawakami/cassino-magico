d0f1703ee778d2bc811b0ffc712b6087a854413b
juliana-kawakami <juliana121204@gmail.com>
2025-02-21 15:37:04 -0300

    question page pronta

--- src/app/question/page.jsx
+++ src/app/question/page.jsx
@@ -1,10 +1,20 @@
-import { ESLINT_DEFAULT_DIRS } from "next/dist/lib/constants";
+
 import "../globals.css";
 
 export default function Question() {
   return (
-    <h1 className="text-golden p-6 text-7xl text-center whitespace-nowrap">
-      QUESTION
-    </h1>
+    <div className="flex flex-col gap-8">
+      <h1 className="text-golden p-6 text-7xl text-center">
+        FÁCIL - JOGOS
+      </h1>
+      <div className="flex flex-col justify-between h-[600px] bg-beige mx-60 rounded-xl">
+        <p className="text-brown font-semibold text-4xl p-14 text-center">
+          Qual foi o primeiro jogo desenvolvido pela Nintendo a apresentar o
+          personagem Mario, lançado originalmente em 1981 para fliperamas?
+        </p>
+        <p className="text-brown text-4xl p-14 text-center">TEMPO DE RESPOSTA: 30 SEG</p>
+      </div>
+      <div></div>
+    </div>
   );
 }
