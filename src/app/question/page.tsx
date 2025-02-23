773620755be2efe2be10ba68c4c23465011cae51
juliana-kawakami <juliana121204@gmail.com>
2025-02-23 17:30:19 -0300

    adiciona timer

--- src/app/question/page.tsx
+++ src/app/question/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
+import { useSearchParams, useRouter } from "next/navigation";
 import data from "../data.json";
 import "../globals.css";
 import { useEffect, useState, Suspense } from "react";
@@ -16,19 +16,47 @@ interface Question {
 function QuestionContent() {
   const searchParams = useSearchParams();
   const id = searchParams.get("id");
+  const router = useRouter();
 
   const [question, setQuestion] = useState<Question | null>(null);
+  const [timeLeft, setTimeLeft] = useState<number | null>(null);
 
   useEffect(() => {
     if (id) {
       // Encontra a pergunta correspondente no JSON
       const questionId = data.questions.find((q) => q.id === parseInt(id));
-      setQuestion(questionId || null);
+      if (questionId) {
+        setQuestion(questionId);
+        setTimeLeft(questionId.time); // Inicializa o tempo restante
+      }
     }
   }, [id]);
 
+  useEffect(() => {
+    if (timeLeft === null || timeLeft <= 0) return;
+
+    const interval = setInterval(() => {
+      setTimeLeft((prevTime) => {
+        if (prevTime === null || prevTime <= 0) {
+          clearInterval(interval); // Para o intervalo se o tempo for nulo ou zero
+          return 0;
+        }
+        return prevTime - 1;
+      });
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [timeLeft]);
+
+  useEffect(() => {
+    if (timeLeft === 0) {
+      // Redireciona para outra página quando o tempo chegar a zero
+      router.push("/time-over"); // Substitua "/outra-pagina" pela rota desejada
+    }
+  }, [timeLeft, router]);
+
   if (!question) {
-    return <div>Pergunta não encontrada. </div>;
+    return <div>Pergunta não encontrada.</div>;
   }
 
   return (
@@ -41,7 +69,7 @@ function QuestionContent() {
           {question.question}
         </p>
         <p className="text-brown text-4xl p-14 text-center">
-          TEMPO DE RESPOSTA: {question.time} SEG
+          TEMPO DE RESPOSTA: {timeLeft !== null ? timeLeft : question.time} SEG
         </p>
       </div>
     </div>
---
+++ src/app/time-over/page.tsx
@@ -0,0 +1,7 @@
+export default function TimeOver() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-golden text-7xl text-center">TEMPO ESGOTADO!!!</h1>
+    </div>
+  );
+}
