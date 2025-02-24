b4cb59ec1e5ab25668371bfb84b492ba49254cf0
juliana-kawakami <juliana121204@gmail.com>
2025-02-23 21:02:12 -0300

    muda tela de time-over

--- src/app/time-over/page.tsx
+++ src/app/time-over/page.tsx
@@ -1,7 +1,11 @@
 export default function TimeOver() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <h1 className="text-golden text-7xl text-center">TEMPO ESGOTADO!!!</h1>
+    <div className="flex items-center justify-center min-h-screen w-full">
+      <div className="flex flex-col w-full justify-center items-center h-[600px] mx-60 bg-beige rounded-xl">
+        <h1 className="w-full text-darkbrown text-7xl text-center">
+          TEMPO ESGOTADO!!!
+        </h1>
+      </div>
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,8 +5,8 @@ import "../app/globals.css"; // Certifique-se de importar o CSS global, se neces
 const Card = ({ points, href, id }) => {
   return (
     <Link href={`${href}?id=${id}`} passHref>
-      <div className="relative flex items-center justify-center w-56 h-32 text-darkbrown text-5xl font-bold">
-        <div className="absolute inset-0 bg-gradient-to-b from-white to-beige clip-hexagon hover:opacity-70"></div>
+      <div className="relative flex items-center justify-center w-56 h-32 text-darkbrown text-5xl font-bold hover:opacity-70">
+        <div className="absolute inset-0 bg-gradient-to-b from-white to-beige clip-hexagon"></div>
         <span className="relative">{points}</span>
       </div>
     </Link>
